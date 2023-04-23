a2dd09d68f8d2815d5ba62198ff34d6061887f12
Maksym Kravchenko <kmaksym@gmail.com>
2023-04-22 17:12:31 -0700

    need to add debounce

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+function fetchCountries(name){
+    const BASE_URL = 'https://restcountries.com/v3.1/name'
+    
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,languages,flags`)
+        .then((resp) => {
+            if(!resp.ok){
+                upgreatMarkup(refs.list.innerHTML, refs.info.innerHTML);
+            throw new Error(resp.status)
+            }
+            return resp.json()
+        })
+}
+
+export {fetchCountries}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 import './css/styles.css';
+import Notiflix from 'notiflix'
+import {fetchCountries} from './fetchCountries'
+import _ from 'lodash'
 
 const DEBOUNCE_DELAY = 300;
-console.log('Hello')
+const input = document.querySelector('input')
+const countryInfo = document.querySelector('.country-info')
+const countryListEL = document.querySelector('.country-list')
+
+input.addEventListener('input', onInput)
+function onInput(ev){
+    currentCountries = ev.currentTarget.value.trim()
+        if(currentCountries.length===0){
+            countryListEL.innerHTML='';
+            countryInfo.innerHTML='';
+        }else if(currentCountries.length<=1){
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
+    }
+    return fetchCountries(currentCountries)
+        .then((data)=>{
+            if(data.length===1){
+                countryListEL.innerHTML='';
+                countryInfo.innerHTML=(createMarkupCountry(data))
+            }else{
+                countryInfo.innerHTML ='';
+                countryListEL.innerHTML=(createMarkupList(data))
+            }}
+    ).catch(err=> console.log('Error',err))
+}
+    
+function createMarkupList(arr){
+    return arr.map(({flags, name: {official}})=> 
+        `<li style="list-style: none;style="display: flex;">
+        <div style="display: flex; align-items: center;">
+        <img style="width:40px; height:30px" src="${flags.svg}" alt="${official}">
+        <p>${official}</p></div></li>`
+        ).join('')
+}
+function createMarkupCountry(arr){
+    return arr.map(({flags, name: {official}, languages, capital, population})=>
+    `<h1><img style="width:40px; height:30px" src="${flags.svg}" alt="${official}"> ${official}</h1>
+    <h2>Capital: <span style="font-weight: normal">${capital}</span></h2>
+    <h2>Population: <span style="font-weight: normal">${population}</span></h2>
+    <h2>Languages: <span style="font-weight: normal">${Object.values(languages)}</span></h2>`
+        ).join('')
+}
